ce03c40339b85074e0bf0fa9a75a28f892ea10b2
abdijabarm <78106274+abdijabarm@users.noreply.github.com>
2021-11-07 17:39:10 +0000

    HLW5Part3
    SQL Data Generation, Manipulation and Conversion

---
+++ HLW5/HLW5Part3.sql
@@ -0,0 +1,7 @@
+set sql_mode='ansi';
+#task 1
+select substring('please find the substring in this string',17,9);
+#task 2 
+select sign(-25.76823),abs(-25.76823),round(-25.76823,1);
+#task 3
+select extract(month from current_date());
